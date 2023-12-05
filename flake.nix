7398c5d5dd20da99f03872549f182fcb8c0726f5
Samuel Evans-Powell <mail@sevanspowell.net>
2023-12-05 10:04:27 +0800

    Clean up flake a bit

--- flake.nix
+++ flake.nix
@@ -15,44 +15,16 @@
       # The platforms you can build from (buildPlatform).
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     in
-      utils.lib.eachSystem supportedSystems (system:
+      (utils.lib.eachSystem supportedSystems (system:
         let
           pkgs = import nixpkgs {
             inherit system;
             inherit (haskellNix) config;
             overlays = [
               haskellNix.overlay
-              (self: super: {
-                # bindings-GLFW uses the "Gdi32" package in it's extra-libraries
-                # clause for MinGW. haskell.nix expects that library to be called
-                # "gdi32" (lowercase).
-                # See
-                # https://input-output-hk.github.io/haskell.nix/tutorials/pkg-map.html#mapping-non-haskell-dependencies-to-nixpkgs
-                # for more information and other ways of doing this.
-                Gdi32 = null;
-                # The vulkan haskell package refers to the vulkan library on
-                # Windows as "vulkan-1". It's true, the vulkan DLL produced by
-                # vulkan-loader is "vulkan-1.dll", but nixpkgs can't use that
-                # information, we need to direct it to vulkan-loader in order to
-                # get the vulkan-1.dll.
-                vulkan-1 = self.vulkan-loader;
-              })
-              (self: super: {
-                # Fix build of GLFW cross-compiled to Windows
-                glfw = self.callPackage ./glfw.nix {
-                  inherit (self.darwin.apple_sdk.frameworks) Carbon Cocoa Kernel OpenGL;
-                };
-                # Fix build of vulkan-loader cross-compiled to Windows
-                vulkan-loader = self.callPackage ./vulkan-loader.nix {
-                  inherit (self.darwin) moltenvk;
-                };
-                # Fix build of vulkan-validation-layers cross-compiled to Windows
-                vulkan-validation-layers = self.callPackage ./vulkan-validation-layers.nix {};
-              })
+              self.overlays.default
             ];
           };
-
-          inherit (pkgs) lib stdenv;
 
           project                   = import ./project.nix pkgs.haskell-nix;
           projectWindowsCross       = import ./project.nix pkgs.pkgsCross.mingwW64.haskell-nix;
@@ -60,14 +32,45 @@
           projectMuslCross          = import ./project.nix pkgs.pkgsCross.musl64.haskell-nix;
           projectDarwinIntelCross   = import ./project.nix pkgs.pkgsCross.x86_64-darwin.haskell-nix;
           projectDarwinARMCross     = import ./project.nix pkgs.pkgsCross.aarch64-darwin.haskell-nix;
-        in
-        {
-          inherit haskellNix pkgs project projectWindowsCross projectWindowsStaticCross projectMuslCross;
-          # Don't do this, we're missing patches from haskell.nix if we go raw nixpkgs route.
-          # x = pkgs.pkgsCross.mingwW64.haskellPackages.bindings-GLFW.overrideAttrs (old: { buildInputs = []; librarySystemDepends = []; });
-        } // lib.optionalAttrs stdenv.buildPlatform.isDarwin {
+        in {
+          inherit nixpkgs project projectWindowsCross projectWindowsStaticCross projectMuslCross;
+
+          legacyPackges = pkgs;
+        } // nixpkgs.lib.optionalAttrs (system == "aarch64-darwin") {
           # Only Mac can cross-compile for darwin.
-          inherit projectDarwinIntelCross projectDarwinARMCross;
+          inherit projectDarwinIntelCross;
+        } // nixpkgs.lib.optionalAttrs (system == "x86_64-darwin") {
+          inherit projectDarwinARMCross;
         }
-      );
+      )) // {
+        overlays.default = final: prev: {
+          # bindings-GLFW uses the "Gdi32" package in it's extra-libraries
+          # clause for MinGW. haskell.nix expects that library to be called
+          # "gdi32" (lowercase).
+          # See
+          # https://input-output-hk.github.io/haskell.nix/tutorials/pkg-map.html#mapping-non-haskell-dependencies-to-nixpkgs
+          # for more information and other ways of doing this.
+          Gdi32 = null;
+
+          # The vulkan haskell package refers to the vulkan library on
+          # Windows as "vulkan-1". It's true, the vulkan DLL produced by
+          # vulkan-loader is "vulkan-1.dll", but nixpkgs can't use that
+          # information, we need to direct it to vulkan-loader in order to
+          # get the vulkan-1.dll.
+          vulkan-1 = final.vulkan-loader;
+
+          # Fix build of GLFW cross-compiled to Windows
+          glfw = final.callPackage ./glfw.nix {
+            inherit (final.darwin.apple_sdk.frameworks) Carbon Cocoa Kernel OpenGL;
+          };
+
+          # Fix build of vulkan-loader cross-compiled to Windows
+          vulkan-loader = final.callPackage ./vulkan-loader.nix {
+            inherit (final.darwin) moltenvk;
+          };
+
+          # Fix build of vulkan-validation-layers cross-compiled to Windows
+          vulkan-validation-layers = final.callPackage ./vulkan-validation-layers.nix {};
+        };
+      };
 }
